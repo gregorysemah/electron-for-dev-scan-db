11a8fe7562d128a62dd0a5a639a1c54feb03f5b4
greg <gregory.semah@gmail.com>
2019-10-23 13:18:23 +0200

    remove useless code

--- main.js
+++ main.js
@@ -3,56 +3,127 @@ const electron = require('electron')
 const path = require('path')
 const mqtt = require('mqtt')
 const {app, BrowserWindow} = electron;
-const server = require('express')();
+const express = require('express');
 const proxy = require('express-http-proxy');
+const exec = require('child_process').exec
+const server = express();
 
-let configID = 6;
+let configID = 4;
+
+let configs = [
+  
+  
+  { 
+    num:0, 
+    mode:"MACRO",
+    title:"MACRO PUPITRE GAUCHE", 
+    ip:"192.168.1.50", 
+    urls:[
+      "/#/macro/pupitre/pupitregauche",
+    ],
+  },
+  { 
+    num:1, 
+    mode:"MACRO",
+    title:"MACRO PUPITRE DROIT", 
+    ip:"192.168.1.51", 
+    urls:[
+      "/#/macro/pupitre/pupitredroit",
+    ], 
+  },
+  { 
+    num:2, 
+    mode:"MICRO",
+    title:"CHAMBRE CLARA", 
+    ip:"?", 
+    urls:[
+      "/#/bedroom/chest",
+      "/#/bedroom/pc-interface",
+    ],
+  },
+  { 
+    num:3, 
+    mode:"MICRO",
+    title:"SALON CLARA", 
+    ip:"192.168.1.35", 
+    urls:[
+      "/#/living-room/chest",
+      "/#/living-room/television",
+    ],
+  },
+  { 
+    num:4, 
+    mode:"MICRO",
+    title:"MALETTE SALON", 
+    ip:"192.168.1.16", 
+    urls:[
+      "/#/headquarter/bedroom-bathroom/database",
+      "/#/headquarter/bedroom-bathroom/scan",
+    ],
+  },
+  { 
+    num:5, 
+    mode:"MICRO",
+    title:"MALETTE CHAMBRE", 
+    ip:"192.168.1.17", 
+    urls:[
+      "/#/headquarter/kitchen-living-room/database",
+      "/#/headquarter/kitchen-living-room/scan",
+    ],
+  },
+  { 
+    num:6, 
+    mode:"MICRO",
+    title:"BRIEFING-ROOM", 
+    ip:"192.168.1.5", 
+    urls:[
+      "/#/briefing/television",
+    ],
+  },
+  { 
+    num:7, 
+    mode:"MICRO",
+    title:"VELLEDA", 
+    ip:"192.168.1.12", 
+    urls:[
+      "/#/corridor/velleda",
+    ],
+  },
+  { 
+    num:8, 
+    mode:"MICRO",
+    title:"TESTS", 
+    ip:"RTC", 
+    urls:[
+      "/#/rtc/emiter/dev",
+      "/#/rtc/receiver/dev",
+    ],
+    
+  }
+]
+
+const environnement = configs[configID].mode;
+let urls = configs[configID].urls;
+
 
 let mqttBrokerUrl = 'mqtt://192.168.1.43:1883';
 let baseUrl = "http://192.168.1.43:8080"
+let serveUrl = "http://localhost:3000"
 
-server.use('/', proxy(baseUrl));
+if(environnement == "MACRO"){
+  mqttBrokerUrl = 'mqtt://192.168.1.44:1883';
+  baseUrl = "http://192.168.1.44:8080"
+  // serveUrl = "file://public"
+  //server.use('/', express.static("public"));
+  server.use('/', proxy(baseUrl));
+}
+else{
+  server.use('/', proxy(baseUrl));
+}
+
 server.listen(3000, function () {})
 
-let configs = [
-  [ //0 MACRO PUPITRE GAUCHE 192.168.1.50
-    "/#/macro/pupitre/pupitregauche",
-  ],
-  [ //1 MACRO PUPITRE DROIT 192.168.1.51
-    "/#/macro/pupitre/pupitredroit",
-  ], 
-  [ //2
-    "/#/bedroom/chest",
-    "/#/bedroom/pc-interface",
-  ],
-  [ //3 SALON CLARA 192.168.1.35
-    "/#/living-room/chest",
-    "/#/living-room/television",
-  ],
-  [ //4 MALETTE SALON 192.168.1.16
-    "/#/headquarter/bedroom-bathroom/database",
-    "/#/headquarter/bedroom-bathroom/scan",
-  ],
-  [ //5 MALETTE CHAMBRE 192.168.1.17
-    "/#/headquarter/kitchen-living-room/database",
-    "/#/headquarter/kitchen-living-room/scan",
-  ],
-  [ //6 BRIEFING-ROOM 192.168.1.130 ou 192.168.1.5
-    "/#/briefing/television",
-  ],
-  [ //7 VELLEDA 192.168.1.12
-    "/#/corridor/velleda",
-  ],
-  [ //8 TESTS RTC
-    "/#/rtc/emiter/dev",
-    "/#/rtc/receiver/dev",
-  ],
-  [ //9 Home
-    "/",
-  ],
 
-]
-let urls = configs[configID];
 
 let windows = [];
 
@@ -61,10 +132,10 @@ function createWindows () {
   const displays = electron.screen.getAllDisplays()
   for(let i = 0; i<urls.length; i++) {
     const url = urls[i];
-      
+    
     const display = displays[Math.min(i,displays.length-1)];
     const { x, y, width, height} = display.workArea;
-
+    
     let window = new BrowserWindow({
       x, y, width, height, // set to display dimensions
       fullscreen: true,
@@ -77,22 +148,22 @@ function createWindows () {
         preload: path.join(__dirname, 'preload.js')
       }
     })
-
+    
     // Open the DevTools.
     // window.webContents.openDevTools()
-
+    
     // and load the index.html of the app.
-    window.loadURL("http://localhost:3000" + url)
-
+    window.loadURL(serveUrl + url)
+    
     // remove security warning
     process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
-
+    
     windows.push(window);
-
+    
     window.once('ready-to-show', () => {
       window.show();
     })
-
+    
     // Emitted when the window is closed.
     window.on('closed', function () {
       // Dereference the window object, usually you would store windows
@@ -132,6 +203,14 @@ function reset() {
   }
 }
 
+function shutdown() {
+  exec("shutdown /s")
+}
+
+
+function restart() {
+  exec("shutdown /r")
+}
 
 
 topicHandlers = [
@@ -140,27 +219,39 @@ topicHandlers = [
     handler:(payload) => {
       reset();
     }
+  }, 
+  {
+    topic:"/computer/shutdown",
+    handler:(payload) => {
+      shutdown();
+    }
+  }, 
+  {
+    topic:"/computer/restart",
+    handler:(payload) => {
+      restart();
+    }
   }
 ]
 
 function handleMessage(topic, message) {
   topicHandler = topicHandlers.find((t=>t.topic == topic))
-
+  
   if(topicHandler){
     topicHandler.handler(message);
   }
 }
 
 mqttClient = mqtt.connect(mqttBrokerUrl);
-        
+
 mqttClient.on("connect", function (status) {
-    console.log("MQTT connected with status: ", status)
+  console.log("MQTT connected with status: ", status)
 })
 
 mqttClient.on("message", function (topic, message) {
   console.log("mqtt message", topic, message)
-
-    handleMessage(topic, message.toString())
+  
+  handleMessage(topic, message.toString())
 })
 
 for (const topicHandler of topicHandlers) {
